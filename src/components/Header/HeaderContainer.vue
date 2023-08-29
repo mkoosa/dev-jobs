c861e6f32b4e83a4560b06c9fcdc97b30bbc9a8d
Marcin Kosecki <marcin.kosecki@int.pl>
2023-08-29 09:45:09 +0200

    Create component HeaderContainer

---
+++ src/components/Header/HeaderContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    <div class="header__content">
+      <header-heading />
+      <switcher-color-schema />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SwitcherColorSchema from "@/components/Shared/SwitcherColorSchema.vue"
+import HeaderHeading from "@/components/Header/HeaderHeading.vue"
+</script>
+
