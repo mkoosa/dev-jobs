de12801de449a9e5eec954884162c4df1bbd1190
Marcin Kosecki <marcin.kosecki@int.pl>
2023-08-29 09:46:08 +0200

    Create component SwitcherColorSChema

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
-
 <template>
   <div class="wrapper">
     <header-container />
   </div>
 </template>
-
 <script setup>
 import HeaderContainer from './components/Header/HeaderContainer.vue';
-
 </script>
-
 
 <style>
 @import url("@/assets/css/main.css");
---
+++ src/components/Shared/SwitcherColorSchema.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="switcher">
+    <img src="../../../src/assets/desktop/icon-sun.svg" alt="sun">
+    <div class="toggle-container" @click="CHANGE_COLOR_SCHEMA">
+      <div class="circle"></div>
+    </div>
+    <font-awesome-icon :class="['moon', 'icon']" :icon="['fas', 'moon']" />
+  </div>
+</template>
+
+<script setup>
+import useColorSchemaStore from '../../stores/colorSchema';
+
+const colorSchemaStore = useColorSchemaStore();
+const CHANGE_COLOR_SCHEMA =  colorSchemaStore.CHANGE_COLOR_SCHEMA;
+</script>
+
+
+
