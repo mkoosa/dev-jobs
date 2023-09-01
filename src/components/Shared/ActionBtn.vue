0aa116127c8daf8b277fd2d2cd316f05e5c40c3a
Marcin Kosecki <marcin.kosecki@int.pl>
2023-09-01 15:18:44 +0200

    Create component ActionBtn

---
+++ src/components/Shared/ActionBtn.vue
@@ -0,0 +1,62 @@
+<template>
+  <button :class="buttonClass">{{ text }}<slot></slot></button>
+</template>
+<script setup>
+import { computed, toRefs } from "vue"
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: false
+  },
+  type: {
+    type: String,
+    required: false
+  }
+})
+
+const { type } = toRefs(props)
+const buttonClass = computed(() => {
+  return {
+    [type.value]: true // primary:type.value === 'primary'
+  }
+})
+</script>
+
+<style scoped>
+.insideIcon,
+.search-btn{
+  width: 4.5rem;
+  height: 4.5rem;
+  color: var(--white);
+  background: var(--violet);
+  border: none;
+  outline: none;
+  border-radius: .5rem;
+}
+
+
+.search-btn{
+  width: 100%;
+  font-size: 1.6rem;
+  letter-spacing: .1rem;
+  font-weight: 700;
+  height: 5.5rem;
+  
+}
+
+.search-btn:focus{
+  outline: .2rem solid var(--grey);
+  outline-offset: .3rem;
+}
+
+@media screen and (min-width: 768px) {
+  .search-btn{
+    width: auto;
+    height: auto;
+    padding: .8em 1em;
+    cursor: pointer;
+  }
+}
+
+</style>
