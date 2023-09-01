b24eec54cddef367ddaaa14fe0fd5246973fdb36
Marcin Kosecki <marcin.kosecki@int.pl>
2023-09-01 15:19:31 +0200

    Create component TextInput

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <input class="form__input" type="text" :value="modelValue" placeholder="Filter by tittle ..." @input="handleInput">
+</template>
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(["update:modelValue"]);
+const handleInput = (event) => {
+  const target = event.target;
+  emit("update:modelValue", target.value)
+}
+
+</script>
+
+<style scoped>
+.form__input {
+  width: 50%;
+  padding: .5em 1em;
+  outline: none;
+  font-size: 1.5rem;
+  border: none;
+  color: var(--grey);
+}
+
+:focus {
+  border-bottom: .2rem solid var(--grey);
+  margin-left: 2rem;
+
+}
+
+@media screen and (min-width:768px) {
+  .form__input {
+    width: 75%;
+  }
+
+}
+</style>
+
+
