fffa646e4236076e5af1bfdde129808ee9ef0601
Marcin Kosecki <marcin.kosecki@int.pl>
2023-09-09 20:00:31 +0200

    Set up testing-library/jest-dom

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
 import { cleanup } from '@testing-library/vue';
 import { afterEach } from 'vitest';
-
+import '@testing-library/jest-dom'
 //cleaning old test before starting the new one
 afterEach(() => cleanup());
