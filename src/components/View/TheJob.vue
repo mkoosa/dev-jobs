f71583bed43e7fef220c226e3f9ded6bf6aaa58d
Marcin Kosecki <marcin.kosecki@int.pl>
2023-09-21 14:09:26 +0200

    Create TheJob component

---
+++ src/components/View/TheJob.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="job">
+    Job {{ currenntJob }}
+  </div>
+  
+</template>
+
+<script setup>
+import {useRoute} from "vue-router"  
+import {computed} from "vue"
+const route = useRoute();
+
+const currenntJob = computed(()=> route.params.id)
+
+
+</script>
+
+
+<style scoped>
+.job{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4rem;
+  height: 40rem;
+}
+
+</style>
