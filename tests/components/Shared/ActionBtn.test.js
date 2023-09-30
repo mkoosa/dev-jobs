da3c540de028307ebab558231425dfffe2ceab50
Marcin Kosecki <marcin.kosecki@int.pl>
2023-09-30 17:35:59 +0200

    Add one more unit test for ActionBtn component

--- tests/components/Shared/ActionBtn.test.js
+++ tests/components/Shared/ActionBtn.test.js
@@ -2,18 +2,32 @@ import { render, screen } from '@testing-library/vue';
 import ActionBtn from '@/components/Shared/ActionBtn.vue';
 import { describe, expect } from 'vitest';
 
-describe('ActiobBtnn', () => {
-  it('render text', () => {
+describe('ActiobBtn', () => {
+
+  const renderActionBtn = () => {
     render(ActionBtn, {
       props: {
         text: 'Search',
         type: 'search-btn'
+      },
+      slots: {
+        default: `<template>
+              <h1>in slots</h1>
+              </template>`
       }
     });
-
+  }
+  it('renders text', () => {
+    renderActionBtn();
     const button = screen.getByRole('button', {
       name: /Search/i
     });
     expect(button).toBeTruthy();
+  });
+
+  it('renders slot content', async () => {
+    renderActionBtn()
+    const text = await screen.findByText('in slots')
+    expect(text).toBeInTheDocument()
   });
 });
