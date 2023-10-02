314c80d9d41d44f26869c2a950f72ec0b8a38431
Marcin Kosecki <marcin.kosecki@int.pl>
2023-10-02 10:52:41 +0200

    Complete unit tests for pagination store

--- tests/unit/stores/pagination.test.js
+++ tests/unit/stores/pagination.test.js
@@ -1,7 +1,7 @@
 import { createPinia, setActivePinia } from 'pinia';
 import { beforeEach, describe, expect } from 'vitest';
 import { jobStore } from '@/main';
-import usePaginationStore from '@/stores/pagination';
+import {paginationStore} from '@/main' ;
 
 describe('getter', () => {
   beforeEach(() => {
@@ -9,9 +9,23 @@ describe('getter', () => {
   });
 
   it('gets TOTAL_PAGES ', () => {
-    const store = usePaginationStore();
     jobStore.jobs.record = Array(36).fill({});
-    const result = store.TOTAL_PAGES;
+    const result = paginationStore.TOTAL_PAGES;
+    jobStore.jobs.record = Array(36).fill({});
     expect(result).toBe(4);
   });
+  it('gets current page', () => {
+    const result = paginationStore.CURRENT_PAGE;
+    expect(result).toBe(1);
+  }),
+  it('gets previous page', () => {
+    const result = paginationStore.PREVIOUS_PAGE;
+    expect(result).toBe(0);
+  })
+  it('gets next page', () => {
+    const result = paginationStore.NEXT_PAGE;
+    expect(result).toBe(2);
+  })
+  
+
 });
